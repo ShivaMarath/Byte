ed3bb50fecc4433ba0d146803c39056e940fe9e0
Shiva Marath <shivamarath2005@gmail.com>
2026-03-17 00:36:58 +0530

    device flow initialization

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
-// This MUST be the first import
+
 import './lib/preload.js';
 
-// Now import everything else - these will run AFTER preload
+
 import express from "express";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
