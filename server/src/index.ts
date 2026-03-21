11428693de44ca7d759823b3e7a2b2abd7556362
Shiva Marath <shivamarath2005@gmail.com>
2026-03-22 03:38:37 +0530

    completed backend-auth

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,6 +23,10 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.send("OK");
 });
+app.get("/device", async (req, res) => {
+  const { user_code } = req.query; 
+  res.redirect(`http://localhost:3000/device?user_code=${user_code}`);
+});
 
 const PORT = process.env.PORT || 3005;
 app.listen(PORT, () => {
